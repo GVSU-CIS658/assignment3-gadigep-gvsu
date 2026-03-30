0bc78872cd934d9fea2e1d1b417619177127ff96
Pooja Gadige <gadigep@mail.gvsu.edu>
2026-03-29 20:08:48 -0400

    Modify Beverage component

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -1,31 +1,32 @@
 <template>
-  <Mug>
-    <Cold v-if="isIced" />
-    <Hot v-else />
-    <Contents>
-      <template v-slot:top>
-        <Creamer />
-      </template>
-      <template v-slot:mid>
-        <Syrup />
-      </template>
-      <template v-slot:bottom>
-        <Base />
-      </template>
-    </Contents>
-  </Mug>
-</template>
-<script setup lang="ts">
-import Contents from "./Contents.vue";
-import Mug from "./Mug.vue";
-import Syrup from "./Syrup.vue";
-import Base from "./Base.vue";
-import Creamer from "./Creamer.vue";
-import Hot from "./Hot.vue";
-import Cold from "./Cold.vue";
+  <div class="mug">
+    <div class="mug-body">
+      <Base :base="base">
+        <!-- Only show syrup if a syrup is chosen -->
+        <Syrup v-if="syrup !== 'none'" :syrup="syrup">
+          <!-- Creamer sits on top of syrup, or directly on base if no syrup -->
+          <Creamer v-if="creamer !== 'none'" :creamer="creamer" />
+        </Syrup>
 
-type Props = {
-  isIced: boolean;
-};
-defineProps<Props>();
+        <!-- If no syrup but creamer chosen: creamer directly on base (no gap) -->
+        <Creamer
+          v-if="creamer !== 'none' && syrup === 'none'"
+          :creamer="creamer"
+        />
+      </Base>
+    </div>
+    <div class="mug-handle"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Base from './Base.vue'
+import Creamer from './Creamer.vue'
+import Syrup from './Syrup.vue'
+
+defineProps<{
+  base: string
+  creamer: string
+  syrup: string
+}>()
 </script>
