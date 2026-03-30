2c9f4716f97c32b36a0eef496939f13718ce2340
Pooja Gadige <gadigep@mail.gvsu.edu>
2026-03-29 20:03:11 -0400

    Modify Creamer component

--- src/components/Creamer.vue
+++ src/components/Creamer.vue
@@ -1,57 +1,15 @@
 <template>
-  <div class="froth">
-    <div v-for=" in 5" class="foam"></div>
-  </div>
+  <div class="creamer-layer" :style="creamStyle"><slot /></div>
 </template>
 
-<script setup lang="ts"></script>
-<style lang="scss" scoped>
-.froth {
-  overflow: visible;
-  transform: translateY(400%);
-  position: relative;
-  height: 20%;
-  width: 100%;
-  background-color: #c6c6c6;
-  animation: pour-tea 2s 2s forwards;
-}
-.foam {
-  display: block;
-  background: #e4e0d2;
-  border-radius: 30px;
-  height: 40px;
-  width: 40px;
-  position: absolute;
-}
+<script setup lang="ts">
+import { computed } from 'vue'
+const props = defineProps<{ creamer: string }>()
 
-.foam:nth-child(1) {
-  top: 0px;
-  left: -3px;
+const colors: Record<string, string> = {
+  'milk':      'rgba(240,240,230,0.85)',
+  'cream':     'rgba(255,253,245,0.92)',
+  'half-half': 'rgba(245,230,200,0.88)',
 }
-
-.foam:nth-child(2) {
-  top: 0px;
-  left: 55px;
-}
-
-.foam:nth-child(3) {
-  width: 30px;
-  height: 30px;
-  border-radius: 40px;
-  top: 3px;
-  left: 30px;
-}
-
-.foam:nth-child(4) {
-  width: 30px;
-  height: 30px;
-  border-radius: 45px;
-  top: 5px;
-  right: -2px;
-}
-
-.foam:nth-child(5) {
-  top: 2px;
-  right: 10px;
-}
-</style>
+const creamStyle = computed(() => ({ background: colors[props.creamer] ?? 'transparent' }))
+</script>
